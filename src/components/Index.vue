440efdd70eaa55546deb240b51075ceb3ce6503e
Guiri6G <58954712+Guiri6G@users.noreply.github.com>
2020-11-12 18:27:03 +0100

    t

---
+++ src/components/Index.vue
@@ -0,0 +1,188 @@
+<template>
+  <div class="index">
+  <!-- <div class="moviePopular">
+    <h2>Film vedette </h2>
+
+      <div class="itemPopular">
+         <section class="popular-area" v-if="listMoviePopular">
+            <div class="w3-content w3-section" v-for="popular in listMoviePopular.items.results" :key="popular.results">
+             <img class="imgSlider" :src="`https://image.tmdb.org/t/p/original${popular.backdrop_path}`" alt="">
+           </div>
+        </section>
+      </div>
+  </div> -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-co-">
+  <a class="navbar-brand" href="#" style="color:red;" >NewFlix</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="#"> Recherche film </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Wishlist</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+    <div class="movinRow" v-for="item in listMovie" :key="item.value">
+      <h2 class="title">{{ item.title }}</h2>
+
+      <div class="list-area">
+        <div class="list">
+          <div class="item" v-for="movie in item.items.results" :key="movie.item">
+            <img :src="`https://image.tmdb.org/t/p/w300${movie.poster_path}`" alt="">
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+import Tmdb  from '../tmdb';
+
+export default {
+  name: 'index',
+
+  data: function(){
+    return{
+      listMovie: null,
+      listMoviePopular: null,
+      imagensPopulas: []
+
+    }
+  },
+
+  props:['dialog'],
+
+  mounted(){
+    Tmdb.getHomeList().then( value => {
+    this.listMovie = value
+    console.log('Liste de film', this.listMovie)
+
+    }).catch(err => {
+      console.log('Erreur !!', err);
+    })
+
+    Tmdb.getMoviePopular().then( value => {
+      this.listMoviePopular = value[0]
+
+      console.log('Liste de film populaire', this.listMoviePopular)
+
+    }).catch(err => {
+      console.log('Erreur sur les films populaires !!', err)
+    })
+
+  },
+
+  watch:{
+    listMoviePopular:function(value){
+      if(value.items.results.length > 0){
+        console.log('Liste de film ')
+        setTimeout(() => {
+          this.carousel();
+        }, 100);
+      }
+    },
+  },
+
+  methods:{
+
+    carousel(){
+      console.log('Appel de fonction')
+      let indexImg = 0
+      let imgLength = 0
+      let img = document.getElementsByClassName('imgSlider');
+
+      for(var i = 0; i < img.length; i++){
+        console.log('Toutes les images',img.length )
+        imgLength = img.length
+        img[i].style.display = 'none';
+      }
+    
+        console.log('INDEX', indexImg)
+
+      img[indexImg].style.display = 'block'
+
+      // indexImg++
+
+      if(indexImg > img.length){
+        indexImg = 0
+      }
+
+      // img[indexImg].style.display = 'none';
+   setInterval(() => {
+       indexImg++
+
+        console.log('index', indexImg)
+
+      img[indexImg-1].style.display = 'none';
+      img[indexImg].style.display = 'block'
+
+      if(indexImg >= imgLength){
+        indexImg = 0
+      }
+
+   }, 6000);
+
+    },
+
+    setCarrouselInterval(){
+
+    
+
+    }
+
+  }
+}
+</script>
+
+<style>
+.movinRow{
+  margin-bottom: 30px;
+}
+.movinRow h2{
+  margin: 0px 0px 0px 30px;
+}
+.list-area{
+  overflow-x: hidden;
+  margin-left: 30px;
+}
+.list{
+  width: 99999999999px;
+}
+.item{
+  display: inline-block;
+  width: 200px;
+  padding: 5px;
+  cursor: pointer;
+}
+.item img{
+  max-width: 100%;
+  transform: scale(0.9);
+  transition: all ease 0.3s;
+}
+.item img:hover{
+  transform: scale(1);
+}
+.popular-area{
+  background-size: 'cover';
+  background-position: 'center';
+}
+.imgSlider{
+  display: none;
+  width: 100%;
+}
+
+.icones{
+  background-color: #ffffff;
+}
+
+</style>
+
+
---
+++ src/tmdb.js
@@ -0,0 +1,83 @@
+const API_KEY = '8026a5cb6390d3443332938c416e8f3f';
+const URL_BASE = 'https://api.themoviedb.org/3';
+
+
+// -originais da netflix
+// -recomendados ( trending )
+// -em alta ( top rated )
+// -ação
+// -Comedia
+// -Terror
+// -romance
+// -documentario
+
+const basicFetch = async (endPoint) => {
+    const req = await fetch(`${URL_BASE}${endPoint}`);
+    const json = await req.json();
+    return json;
+}
+
+export default{
+    getHomeList: async () => {
+        return [
+            {
+                slug: 'Original',
+                title: 'Film originaux de NewFlix',
+                items: await basicFetch(`/discover/tv?with_network=213&language=en-US&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'Latest',
+                title: 'Les nouveautés',
+                items: await basicFetch(`/movie/now_playing?api_key=${API_KEY}&language=en-US`) // le /movie/latest ne fonctionne pas
+            },
+            {
+                slug: 'Upcoming',
+                title: 'À venir',
+                items: await basicFetch(`/movie/upcoming?api_key=${API_KEY}&language=en-US`)
+            },
+            {
+                slug: 'Trend',
+                title: 'Les plus regardés en ce moment',
+                items: await basicFetch(`/trending/all/week?language=en-US&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'TopRated',
+                title: 'Les mieux notés',
+                items: await basicFetch(`/movie/top_rated?language=en-US&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'action',
+                title: 'Action',
+                items: await basicFetch(`/discover/movie?with_genres=28&language=en-US&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'comedy',
+                title: 'Comédie',
+                items:await basicFetch(`/discover/movie?with_genres=35&language=en-US&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'romance',
+                title: 'Romance',
+                items:await basicFetch(`/discover/movie?with_genres=27&language=en-US&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'horror',
+                title: 'Horreur',
+                items:await basicFetch(`/discover/movie?with_genres=10749&language=en-US&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'documentary',
+                title: 'Documentaire',
+                items:await basicFetch(`/discover/movie?with_genres=99&language=en-US&api_key=${API_KEY}`)
+            },
+        ]
+    },
+
+    getMoviePopular: async () => {
+        return [
+            {
+                items: await basicFetch(`/tv/popular?language=en-US&api_key=${API_KEY}`)
+            }
+        ]
+    }
+}
